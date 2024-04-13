b2e4490f757991e61a2bc519bfe781fd5d79c820
adanvi <158237032+adanvi@users.noreply.github.com>
2024-04-12 22:51:11 -0700

    Practicas agregadas 2

---
+++ Ejercicio 26/Ejercicio_7Maravillas/7maravillas.js
@@ -0,0 +1,83 @@
+/*Guarda las imagenes en un arreglo*/
+let maravillas = ["chichen-itza.svg", "coliseo.svg", "cristo-redentor.svg", "machu-picchu.svg", "muralla-china.svg","petra.svg","taj-mahal.svg"];
+/*Guarda las respuestas correctas en un arreglo*/
+let correcta = [1,0,2,1,2,0,1];
+
+/*Crea yn arreglo*/
+let opciones = [];
+/*Guarda pusibles respuestas para que el usuario elija*/
+opciones.push(["Machu Picchu", "Chichen Itza", "Coliseo"]);
+opciones.push(["Coliseo", "Petra", "Cristo Redentor"]);
+opciones.push(["Muralla China", "Taj Mahal", "Cristo Redentor"]);
+opciones.push(["Petra", "Machu Picchu", "Taj Mahal"]);
+opciones.push(["Chichen Itza", "Petra", "Muralla China"]);
+opciones.push(["Petra", "Machu Pichu", "Chichen Itza"]);
+opciones.push(["Coliseo", "Taj Mahal", "Cristo Redentor"]);
+/*Variable para indicar la posicicion donde se encuentra el arreglo*/
+let posActual = 0;
+/*Guarda las cantidades que el jugador a acertado*/
+let cantidadAcertadas = 0;
+
+/*Funcion que da inicio el juego*/
+function comenzarJuego(){
+    posActual = 0;
+    cantidadAcertadas = 0;
+    document.getElementById("pantalla-inicial").style.display = "none";
+    document.getElementById("pantalla-juego").style.display = "block";
+    cargarMaravilla();
+
+}
+
+/*Esta funcion sirve para que cargue las imagenes y las posibles respuestas*/
+function cargarMaravilla(){
+    if(maravillas.length <= posActual){
+        terminarJuego();
+    }
+    else{
+        limpiarOpciones();
+
+        document.getElementById("imgMaravilla").src = "img/" + maravillas[posActual];
+        document.getElementById("n0").innerHTML = opciones[posActual][0];
+        document.getElementById("n1").innerHTML = opciones[posActual][1];
+        document.getElementById("n2").innerHTML = opciones[posActual][2];
+    }
+}
+
+/*Encargado de cambiar las respuestas e imagenes al momento que el ususario alla respondido*/
+function limpiarOpciones(){
+    document.getElementById("n0").className = "nombre";
+    document.getElementById("n1").className = "nombre";
+    document.getElementById("n2").className = "nombre";
+    document.getElementById("l0").className = "letra";
+    document.getElementById("l1").className = "letra";
+    document.getElementById("l2").className = "letra";
+}
+
+/*Funcion que ayuda a comprar la resputas que a elegido el usuario*/
+function comprobarRespuesta(opElegida){
+    if(opElegida==correcta[posActual]){//acertó
+        document.getElementById("n" + opElegida).className = "nombre nombreAcertada";
+        document.getElementById("l" + opElegida).className = "letra letraAcertada";
+        cantidadAcertadas++;
+    }else{
+        document.getElementById("n" + opElegida).className = "nombre nombreNoAcertada";
+        document.getElementById("l" + opElegida).className = "letra letraNoAcertada";
+        document.getElementById("n" + correcta[posActual]).className = "nombre nombreAcertada";
+        document.getElementById("l" + correcta[posActual]).className = "letra letraAcertada";
+    }
+    posActual++;
+    setTimeout(cargarMaravilla,1000);
+}
+/*Muestra el resumen de ascertado*/
+function terminarJuego(){
+    document.getElementById("pantalla-juego").style.display = "none";
+    document.getElementById("pantalla-final").style.display = "block";
+    document.getElementById("numCorrectas").innerHTML = cantidadAcertadas;
+    document.getElementById("numIncorrectas").innerHTML = maravillas.length - cantidadAcertadas;
+}
+ /*Ayuda que Vuelva los menus de inicio si la necesidad de recargar la pagina*/
+function volverAlInicio(){
+    document.getElementById("pantalla-final").style.display = "none";
+    document.getElementById("pantalla-inicial").style.display = "block";
+    document.getElementById("pantalla-juego").style.display = "none";
+}
